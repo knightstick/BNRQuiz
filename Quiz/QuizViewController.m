582d6bf1a1cd01dba1f04a9f09d2c1959145e001
Chris Jewell <chrisjohnjewell@gmail.com>
2015-02-08 11:47:37 +1100

    Manually add a view controller and four views - two labels and two buttons

---
+++ Quiz/QuizViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuizViewController.m
+//  Quiz
+//
+//  Created by Test Mac on 8/02/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@end
+
+@implementation QuizViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
