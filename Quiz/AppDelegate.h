b60cbbb0eb77c7ea1deb7ec3c3cbd553f7d5ab0d
Chris Jewell <chrisjohnjewell@gmail.com>
2015-02-08 11:37:41 +1100

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Test Mac on 8/02/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Test Mac on 8/02/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
