21573ea359347dd46817eb8e8f266fe70ce9b89c
Chris Jewell <chrisjohnjewell@gmail.com>
2015-02-08 12:17:35 +1100

    Initialise the View Controller with questions and answer models

--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -10,12 +10,33 @@
 
 @interface QuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
 @end
 
 @implementation QuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implementated by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        self.questions = @[@"From what is cognac made?", @"What is 7 + 7?", @"What is the captial of Vermont?"];
+        self.answers = @[@"Grapes", @"14", @"Montpelier"];
+    }
+    
+    // Return the address of the new object
+    return self;
+}
 
 - (IBAction)showQuestion:(id)sender
 {
